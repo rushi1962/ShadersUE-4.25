120307116b5e3d08004e1c3bc0e2a359fc2d0e0a
rushi1962 <67573186+rushi1962@users.noreply.github.com>
2023-02-18 10:44:13 +0530

    Project Creation

---
+++ Shaders/Source/Shaders/Shaders.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Shaders.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Shaders, "Shaders" );
